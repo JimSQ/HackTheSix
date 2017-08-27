66761c764dc8b938d0118437416aff4f235091cc
Jim Zhu <flappyboby@gmail.com>
2017-08-27 02:53:07 -0400

    Looks nicer now

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,26 +1,19 @@
 var accessToken = "f5c36cec32f64f61a0fdf426d25fce51",
     investApiKey = "YTG3TTH2PRUNCIVQ",
     baseUrl = "https://api.api.ai/v1/",
-    $speechInput,
-    $recBtn,
-    recognition,
-
-    messageRecording = "Recording...",
-    messageCouldntHear = "I couldn't hear you, could you say that again?",
-    messageInternalError = "Oh no, there has been an internal server error",
-    messageSorry = "I'm sorry, I don't have the answer to that yet.";
+    $input
 
 $(document).ready(function() {
-    $speechInput = $("#speech");
+    $input = $("#input");
     $recBtn = $("#rec");
-    $speechInput.keypress(function(event) {
+    $input.keypress(function(event) {
         if (event.which == 13) {
             event.preventDefault();
             send();
         }
     });
     $recBtn.on("click", function(event) {
-        switchRecognition();
+        send();
     });
     $(".debug__btn").on("click", function() {
         $(this).next().toggleClass("is-active");
@@ -28,58 +21,18 @@ $(document).ready(function() {
     });
 });
 
-function startRecognition() {
-    recognition = new webkitSpeechRecognition();
-    recognition.continuous = false;
-    recognition.interimResults = false;
-    recognition.onstart = function(event) {
-        respond(messageRecording);
-        updateRec();
-    };
-    recognition.onresult = function(event) {
-        recognition.onend = null;
-        var text = "";
-        for (var i = event.resultIndex; i < event.results.length; ++i) {
-            text += event.results[i][0].transcript;
-        }
-        setInput(text);
-        stopRecognition();
-    };
-    recognition.onend = function() {
-        respond(messageCouldntHear);
-        stopRecognition();
-    };
-    recognition.lang = "en-US";
-    recognition.start();
-}
-
-function stopRecognition() {
-    if (recognition) {
-        recognition.stop();
-        recognition = null;
-    }
-    updateRec();
-}
-
-function switchRecognition() {
-    if (recognition) {
-        stopRecognition();
-    } else {
-        startRecognition();
-    }
-}
-
-function setInput(text) {
-    $speechInput.val(text);
-    send();
-}
-
 function updateRec() {
     $recBtn.text(recognition ? "Stop" : "Speak");
 }
 
 function send() {
-    var text = $speechInput.val();
+    var text = $input.val();
+    $("#spokenResponse").addClass("is-active").find(".spoken-response__text").append("[User]: " + text + "\n");
+    $("#logo").css("opacity", 0);
+    if (text == "") {
+        respond("", "Please type something...");
+        return;
+    }
     $.ajax({
         type: "POST",
         url: baseUrl + "query",
@@ -93,21 +46,31 @@ function send() {
             prepareResponse(data);
         },
         error: function() {
-            respond(messageInternalError);
+            respond("", "An Error has Occurred.");
         }
     });
 }
 
 function prepareResponse(val) {
     var debugJSON = JSON.stringify(val, undefined, 2);
-    callAPI(val);
-    //- respond(spokenResponse);
     debugRespond(debugJSON);
+    callAPI(val);
 }
 
+function isEmpty(object) { for (var i in object) { return true; } return false; }
+
 function callAPI(val) {
+    console.log(val);
+    if (Object.getOwnPropertyNames(val).length == 0) {
+        respond("", "Please Type Something...");
+        return;
+    }
+    if (Object.getOwnPropertyNames(val.result.parameters).length == 0) {
+        respond("", val.result.speech);
+        return;
+    }
     if (val.result.parameters.symbol == "") {
-        respond("", "Bad Values...");
+        respond("", val.result.speech);
         return;
     }
     var params = val.result.parameters.params;
@@ -117,30 +80,24 @@ function callAPI(val) {
         urlExt += "&" + arr[i] + "=" + val.result.parameters[arr[i]];
     }
     urlExt = urlExt.substring(1);
-    console.log(urlExt);
     var location = val.result.parameters.location;
     var URL = "https://www.alphavantage.co/query?" + urlExt + "&apikey=" + investApiKey
     var apiData;
     $.get(URL, function(data, status) {
         arr = location.split(',');
         for (var i = 0; i < arr.length; i++) {
-            console.log(JSON.stringify(data));
             if (arr[i][0] == '#') {
                 if (val.result.parameters[arr[i].substring(1)] != null) {
-                    console.log(":*)" + data[val.result.parameters[arr[i].substring(1)]]);
                     if (data[val.result.parameters[arr[i].substring(1)]] == undefined) {
-                        console.log("undefined :)");
                         data = data[Object.keys(data)[0]];
                     } else {
                         data = data[val.result.parameters[arr[i].substring(1)]];
                     }
                 } else {
                     //Is null, go first
-                    console.log("kl");
                     data = data[Object.keys(data)[0]];
                 }
             } else {
-                console.log(arr[i]);
                 if (data[arr[i]] == undefined) {
                     data = data[Object.keys(data)[0]];
                 } else {
@@ -149,29 +106,17 @@ function callAPI(val) {
             }
         }
         apiData = JSON.stringify(data, undefined, 2);
-        //- alert("URL:" + URL + " Data: " + apiData + "\nStatus: " + status);
         respond(apiData, val.result.speech);
     });
-}
-
-function parseParams(val) {
-
 }
 
 function debugRespond(val) {
     $("#response").text(val);
 }
 
-function respond(val, template) {
-    if (val == "") {
-        val = messageSorry;
+function respond(response, template) {
+    if (response == "") {
+        response = "Please type something...";
     }
-    if (val !== messageRecording) {
-        var msg = new SpeechSynthesisUtterance();
-        msg.voiceURI = "native";
-        msg.text = val;
-        msg.lang = "en-US";
-        window.speechSynthesis.speak(msg);
-    }
-    $("#spokenResponse").addClass("is-active").find(".spoken-response__text").html(template.replace("$output", val.replace('"', "$").replace('"', ".")));
+    $("#spokenResponse").addClass("is-active").find(".spoken-response__text").append("[Investerbot]: " + template.replace("$output", response.replace('"', "$").replace('"', ".")) + "\n");
 }
